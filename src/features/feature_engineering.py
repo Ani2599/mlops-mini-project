694510d18f57b57439a93de08aba62d6d0cc85f0
Aniket Nandanwar <aniketnandanwar09@gmail.com>
2024-08-01 11:32:55 +0530

    added flask app

--- src/features/feature_engineering.py
+++ src/features/feature_engineering.py
@@ -1,10 +1,11 @@
+# feature engineering
 import numpy as np
 import pandas as pd
 import os
 from sklearn.feature_extraction.text import CountVectorizer
 import yaml
 import logging
-import joblib  # Add this import to save the vectorizer
+import pickle
 
 # logging configuration
 logger = logging.getLogger('feature_engineering')
@@ -73,11 +74,11 @@ def apply_bow(train_data: pd.DataFrame, test_data: pd.DataFrame, max_features: i
         test_df = pd.DataFrame(X_test_bow.toarray())
         test_df['label'] = y_test
 
-        # Save the vectorizer
-        os.makedirs(os.path.dirname('models/vectorizer.pkl'), exist_ok=True)
-        joblib.dump(vectorizer, 'models/vectorizer.pkl')
-        logger.debug('Bag of Words applied and data transformed, vectorizer saved')
+        pickle.dump(vectorizer, open('models/vectorizer.pkl', 'wb'))
 
+
+
+        logger.debug('Bag of Words applied and data transformed')
         return train_df, test_df
     except Exception as e:
         logger.error('Error during Bag of Words transformation: %s', e)
